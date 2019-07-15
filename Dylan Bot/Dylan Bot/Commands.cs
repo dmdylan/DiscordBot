523160a91ae153cbebc7901c413bfe6102b2c617
dmdylan <51095343+dmdylan@users.noreply.github.com>
2019-07-14 20:58:43 -0400

    Add files via upload

---
+++ Dylan Bot/Dylan Bot/Commands.cs
@@ -0,0 +1,49 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using System.Threading.Tasks;
+using System.IO;
+using System;
+using DSharpPlus.Net;
+
+namespace Dylan_Bot
+{
+    class Commands
+    {
+
+        [Command("hi")]
+        public async Task Hi(CommandContext ctx)
+        {
+            await ctx.TriggerTypingAsync();
+            await ctx.RespondAsync($"👋 Hi, {ctx.User.Mention}!");
+        }
+        
+        [Command("grats")]
+        public async Task Congratulations(CommandContext ctx)
+        {
+            await ctx.TriggerTypingAsync();
+            await ctx.RespondAsync("https://tenor.com/uEWd.gif");          
+        }
+
+        [Command("yes")]
+        public async Task YesYesYes(CommandContext ctx)
+        {
+            await ctx.TriggerTypingAsync();
+            await ctx.RespondAsync("https://i.imgur.com/Qgl3Q2K.gif");
+        }
+
+        /*
+        [Command("frank")]
+        public async Task FrankWipedUs(CommandContext command)
+        {
+            frankWipeCounter++;            
+            await command.RespondAsync($"Frank has caused {frankWipeCounter} wipes 💩");
+        }
+
+        [Command("wipe")]
+        public async Task WeWiped(CommandContext command)
+        {
+            await command.RespondAsync($"The group has wiped {wipeCounter} times");
+        }
+        */
+    }
+}
---
+++ Dylan Bot/Dylan Bot/Program.cs
@@ -0,0 +1,84 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.Interactivity;
+using DSharpPlus.Net.WebSocket;
+using System.Threading.Tasks;
+using DSharpPlus.Entities;
+
+namespace Dylan_Bot
+{
+    public class Program
+    {
+        static DiscordClient discord;
+        static CommandsNextModule commands;
+        //static InteractivityModule interactivity;
+
+        static void Main(string[] args)
+        {
+            MainAsync(args).ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync(string[] args)
+        {
+            discord = new DiscordClient(new DiscordConfiguration
+            {
+                Token = "NTg1NjIwMjAxOTE2MjAzMDI4.XPcg8Q.HRvy2J3vJtvE7HA - kZahdpB9REk",
+                TokenType = TokenType.Bot,
+                UseInternalLogHandler = true,
+                LogLevel = LogLevel.Debug
+            });
+
+            discord.SetWebSocketClient<WebSocket4NetCoreClient>();          
+
+            discord.MessageCreated += async e =>
+            {
+                if (e.Message.Content.ToLower().StartsWith("ping"))
+                    await e.Message.RespondAsync("pong!");
+            };
+
+            discord.MessageCreated += async e =>
+            {
+                if (!(e.Message.Author.IsBot) && e.Message.Content.ToLower().Contains("hackerman"))
+                    await e.Message.RespondAsync("You mean HACKERMAN! " +
+                        "https://gph.is/1JSLT6V");
+            };
+
+            discord.MessageCreated += async e =>
+            {
+                var emoji = DiscordEmoji.FromName(discord, ":thumbsup:");
+                if (e.Message.Content.Contains(emoji) && !(e.Message.Author.IsBot))
+                    await e.Message.RespondAsync(emoji);
+            };
+
+            discord.MessageCreated += async e =>
+            {
+                var emoji = DiscordEmoji.FromGuildEmote(discord, 409841266436734976);
+                if (e.Message.Content.Contains(emoji) && !(e.Message.Author.IsBot))
+                {
+                    await e.Message.RespondAsync(emoji);
+                    await e.Message.CreateReactionAsync(emoji);
+                }                  
+                else if (e.Message.Content.Contains(emoji))
+                {
+                    await e.Message.CreateReactionAsync(emoji);
+                }                   
+            };
+
+            discord.MessageCreated += async e =>
+            {              
+                if (e.Message.Content.ToLower().Contains("thanks bot"))
+                    await e.Message.RespondAsync("👍");
+            };
+
+            commands = discord.UseCommandsNext(new CommandsNextConfiguration
+            {
+                StringPrefix = "d!"
+            });
+
+            commands.RegisterCommands<Commands>();
+
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
+        }
+    }
+}
